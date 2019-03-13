b1c8abe7aa3830bcff6fa8c1052453df5315d6dd
Slawomir Leski <slawomir.leski@gmail.com>
2019-03-13 17:48:52 +0100

    Initial commit.

---
+++ src/main/java/it/tostao/botscheduler/BotSchedulerApplication.java
@@ -0,0 +1,13 @@
+package it.tostao.botscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/tostao/botscheduler/BotSchedulerApplicationTests.java
@@ -0,0 +1,16 @@
+package it.tostao.botscheduler;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BotSchedulerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
